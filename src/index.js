33c82f79566bab9288d636783f8978d908ed6eb6
Andrew Pasquale <andrew@elytra.net>
2024-07-18 12:48:33 -0400

    Inital QA bot implementation

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const domNode = document.createElement('div');
+const root = ReactDOM.createRoot(domNode); 
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+document.body.appendChild(domNode); 
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
