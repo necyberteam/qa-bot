0da039a5e17e31c4d823c4c1f0a353e340d4f2ee
Joe Bacal <bacalj@gmail.com>
2025-05-21 13:20:09 -0400

    wip: simplest development harness

--- src/index.js
+++ src/index.js
@@ -9,14 +9,16 @@ import reportWebVitals from './reportWebVitals';
 // This enables the non-React integration methods demonstrated in the top-level index.html
 window.qAndATool = qAndATool;
 
-// Standard CRA rendering - simplified for demo
+// simulated consuming app's user state
+// In current usage, disabled is a function of isLoggedIn
+// But that will not always be the case
+// The bot will eventually have some functionality when not logged in
+const userLoggedIn = false;
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App
-      disabled={false}
-      isLoggedIn={false}
-    />
+    <App disabled={!userLoggedIn} />
   </React.StrictMode>
 );
 
